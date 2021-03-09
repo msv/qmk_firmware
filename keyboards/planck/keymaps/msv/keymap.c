829bd938272e1c17932e3fc76f5d5b151edadff4
Matt Vaznaian <matt@workonmainstreet.com>
2021-03-09 10:49:13 -0800

    colon to lower, ent on tap lower

--- keyboards/planck/keymaps/msv/keymap.c
+++ keyboards/planck/keymaps/msv/keymap.c
@@ -49,18 +49,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * ,-----------------------------------------------------------------------------------------------.
   * | Tab       |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp       |
   * |-----------+------+------+------+------+------+------+------+------+------+------|------------|
-  * | (LCTL)Esc |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  | RCMD(Enter)|
+  * | (LCTL)Esc |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  | RCMD(')    |
   * |-----------+------+------+------+------+------+------+------+------+------+------+------------|
-  * | Shift     |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | RSFT(')    |
+  * | Shift     |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | RSFT(|)    |
   * |-----------+------+------+------+------+------+------+------+------+------+------+------------|
   * | Ctrl      |   FN | Alt  | GUI  |Lower | Space Layer |Raise | Left | Down |  Up  |  Right     |
   * `----------------------------------------------------------------------------------+------------'
   */
   [_QWERTY] = LAYOUT_planck_grid(
-      KC_TAB,          KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,     KC_Y,     KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC,
-      LCTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,   KC_G,     KC_H,     KC_J,   KC_K,    KC_L,    KC_SCLN, RCMD_T(KC_ENT),
-      KC_LSFT,         KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,     KC_N,     KC_M,   KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_QUOT),
-      KC_LCTL,         FN,      KC_LALT, KC_LGUI, LOWER,  SPACE_FN, SPACE_FN, RAISE,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+      KC_TAB,          KC_Q,    KC_W,    KC_E,    KC_R,              KC_T,     KC_Y,     KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC,
+      LCTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,              KC_G,     KC_H,     KC_J,   KC_K,    KC_L,    KC_SCLN, RCMD_T(KC_QUOT),
+      KC_LSFT,         KC_Z,    KC_X,    KC_C,    KC_V,              KC_B,     KC_N,     KC_M,   KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_PIPE),
+      KC_LCTL,         FN,      KC_LALT, KC_LGUI, LT(LOWER, KC_ENT), SPACE_FN, SPACE_FN, RAISE,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
   ),
 
   /* Lower
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * |------+------+------+------+------+-------------+------+------+------+------+------|
   * |      |      |      |      |      |      |   *  |   4  |   5  |   6  |   -  |Enter |
   * |------+------+------+------+------+------|------+------+------+------+------+------|
-  * |      |      |      |      |      |      |   /  |   1  |   2  |   3  |   +  |      |
+  * |      |      |      |      |      |      |   /  |   1  |   2  |   3  |   +  |Colon |
   * |------+------+------+------+------+------+------+------+------+------+------+------|
   * |      |      |      |      |      |    Space    |      |   0  |   .  |   =  |      |
   * `-----------------------------------------------------------------------------------'
@@ -77,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_LOWER] = LAYOUT_planck_grid(
       LALT(KC_BSPC), KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,     KC_7,     KC_8,    KC_9,     KC_0,     KC_BSPC, 
       XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_ASTR,  KC_4,     KC_5,    KC_6,     KC_MINS,  MT(MOD_LSFT, KC_ENT), 
-      _______,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SLSH,  KC_1,     KC_2,    KC_3,     KC_PLUS,  XXXXXXX, 
+      _______,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SLSH,  KC_1,     KC_2,    KC_3,     KC_PLUS,  S(KC_SCLN), 
       _______,       XXXXXXX, _______, _______, _______, KC_SPC,  KC_SPC,   _______,  KC_0,    KC_DOT,   KC_EQL,   XXXXXXX
   ),
 
