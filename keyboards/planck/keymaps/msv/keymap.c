0867210447e4bc54ad75d8a3ac33869a1dab182e
Matt Vaznaian <matt@workonmainstreet.com>
2021-03-12 09:56:58 -0800

    swipity swap

--- keyboards/planck/keymaps/msv/keymap.c
+++ keyboards/planck/keymaps/msv/keymap.c
@@ -58,8 +58,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   */
   [_QWERTY] = LAYOUT_planck_grid(
       KC_TAB,          KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,     KC_Y,     KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC,
-      LCTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,  KC_G,     KC_H,     KC_J,   KC_K,    KC_L,    KC_SCLN, MT(MOD_RGUI, KC_ENT),
-      KC_LSFT,         KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,     KC_N,     KC_M,   KC_COMM, KC_DOT,  KC_SLSH, MT(MOD_RSFT, KC_QUOT),
+      LCTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,  KC_G,     KC_H,     KC_J,   KC_K,    KC_L,    KC_SCLN, MT(MOD_RSFT, KC_QUOT),
+      KC_LSFT,         KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,     KC_N,     KC_M,   KC_COMM, KC_DOT,  KC_SLSH, MT(MOD_RGUI, KC_ENT),
       KC_LCTL,         FN,      KC_LALT, KC_LGUI, LOWER, SPACE_FN, SPACE_FN, RAISE,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
   ),
 
@@ -76,8 +76,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   */
   [_LOWER] = LAYOUT_planck_grid(
       LALT(KC_BSPC), KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,     KC_7,     KC_8,    KC_9,     KC_0,     KC_BSPC, 
-      XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_ASTR,  KC_4,     KC_5,    KC_6,     KC_MINS,  MT(MOD_LSFT, KC_ENT), 
-      _______,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SLSH,  KC_1,     KC_2,    KC_3,     KC_PLUS,  S(KC_SCLN), 
+      XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_ASTR,  KC_4,     KC_5,    KC_6,     KC_MINS,  S(KC_SCLN), 
+      _______,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SLSH,  KC_1,     KC_2,    KC_3,     KC_PLUS,  KC_ENT, 
       _______,       XXXXXXX, _______, _______, _______, KC_SPC,  KC_SPC,   _______,  KC_0,    KC_DOT,   KC_EQL,   XXXXXXX
   ),
 
@@ -94,8 +94,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   */
   [_RAISE] = LAYOUT_planck_grid(
       KC_TILD,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR, KC_ASTR, KC_UNDS, KC_PIPE, KC_BSPC, 
-      XXXXXXX,  KC_GRV,  KC_PLUS, KC_SLSH, KC_LCBR,  KC_LBRC,  KC_RBRC,  KC_RCBR, KC_BSLS, KC_MINS, KC_EQL,  MT(MOD_LSFT, KC_ENT), 
-      XXXXXXX,  KC_PGDN, KC_PGUP, XXXXXXX, KC_LPRN,  XXXXXXX,  XXXXXXX,  KC_RPRN, XXXXXXX, XXXXXXX, KC_PLUS, XXXXXXX, 
+      XXXXXXX,  KC_GRV,  KC_PLUS, KC_SLSH, KC_LCBR,  KC_LBRC,  KC_RBRC,  KC_RCBR, KC_BSLS, KC_MINS, KC_EQL,  XXXXXXX, 
+      XXXXXXX,  KC_PGDN, KC_PGUP, XXXXXXX, KC_LPRN,  XXXXXXX,  XXXXXXX,  KC_RPRN, XXXXXXX, XXXXXXX, KC_PLUS, KC_ENT, 
       _______,  XXXXXXX, _______, _______, _______,  KC_SPC,   KC_SPC,   _______, KC_HOME, XXXXXXX, XXXXXXX, KC_END
   ),
 
